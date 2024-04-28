e87b7ec715002d9181be6a4bef28fa0409d85078
sobhan <kiko2723@gmial.com>
2024-04-28 14:57:52 +0330

    weblog-header

--- src/Pages/Weblog.jsx
+++ src/Pages/Weblog.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './CSS/Weblog.css'
 
 const Weblog = () => {
+
+    const [like, setLike] = useState(0);
+
     return (
         <div className="weblog">
             <div className="weblog-header">
                 <div className="weblog-header-right">
                     <p className="weblog-header-right-text">وبلاگ</p>
-                    <h1>وبلاگ های متفاوت و مفید</h1>
+                    <h1>بلاگ های متفاوت و مفید</h1>
                     <div className="weblog-header-right-icon">
                         <div className="header-icon">
                             <i className="fa fa-calendar"></i>
@@ -19,13 +22,13 @@ const Weblog = () => {
                         </div>
                         <div className="header-icon">
                             <i className='fa fa-comment'></i>
-                            <p>1 دیدگاه</p>
+                            <p>0 دیدگاه</p>
                         </div>
                     </div>
                 </div>
-                <div className="weblog-header-left">
+                <div onClick={() => setLike(like + 1)} className="weblog-header-left">
                     <i className='fa fa-heart'></i>
-                    <p>1 لایک</p>
+                    <p>{like} لایک</p>
                 </div>
             </div>
         </div>
