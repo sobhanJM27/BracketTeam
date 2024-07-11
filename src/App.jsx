8bc537e236c1aa77c511e616a916cc78552b4e51
sobhan <kiko2723@gmial.com>
2024-07-12 02:58:57 +0330

    logo

--- src/App.jsx
+++ src/App.jsx
@@ -11,26 +11,11 @@ import Service from "./Pages/Service";
 import ScrollToTop from "./Components/ScrollToTop/ScrollToTop";
 import NotFound from "./Pages/NotFound";
 import Weblog from "./Pages/Weblog";
-import { Helmet } from "react-helmet";
 
 function App() {
 
   return (
     <div>
-      <Helmet>
-        <title>bracetteam</title>
-        <meta name="description" content="توضیحات سایت شما" />
-        <script type="application/ld+json">
-        {
-          {
-            "@context": "http://schema.org",
-            "@type": "Organization",
-            "url": "https://bracketteam.net/",
-            "logo": "./public/bracket.png"
-          }
-        }
-        </script>
-      </Helmet>
       <Router>
         <Navbar />
         <ScrollToTop />
