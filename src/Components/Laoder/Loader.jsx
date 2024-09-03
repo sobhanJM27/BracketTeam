de4a585bf0f717cc11bbd6298f61829b16513dc5
sobhan <kiko2723@gmial.com>
2024-09-03 23:33:29 +0330

    9/3/2024

--- src/Components/Laoder/Loader.jsx
+++ src/Components/Laoder/Loader.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 import "./Loader.css";
-import { InfinitySpin } from 'react-loader-spinner';
 
 const Loader = () => {
     return (
         <div className="loader-container">
-            <InfinitySpin
-                visible={true}
-                width="200"
-                color="var(--secondary-color)"
-                ariaLabel="infinity-spin-loading"
-            />
-
+            <div className="loader">
+                <div className="dot"></div>
+                <div className="dot"></div>
+                <div className="dot"></div>
+                <div className="dot"></div>
+            </div>
         </div>
     );
 };
