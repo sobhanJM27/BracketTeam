41225c846175c55001e5f7e345a1b490922796d7
sobhan <kiko2723@gmial.com>
2024-09-03 16:06:55 +0330

    9/3/2024

--- src/Components/FaqPage/FaqPage.jsx
+++ src/Components/FaqPage/FaqPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './FaqPage.css';
-import { faqItems } from '../../Constants/faqItems';
+import { faqItems1, faqItems2 } from '../../Constants/faqItems';
 import ServicesContent from '../ServicesContent/ServicesContent';
 
 const FaqPage = () => {
@@ -15,31 +15,58 @@ const FaqPage = () => {
         <div className="faqpage">
             <h2>سوالات متداول</h2>
             <div className="faqpage-contents">
-                {
-                    faqItems.map((item) => {
-                        const isOpen = item.key === openIndexes;
-                        return (
-                            <div
-                                key={item.key}
-                                className="faqpage-content"
-                            >
+                <div>
+                    {
+                        faqItems1.map((item) => {
+                            const isOpen = item.key === openIndexes;
+                            return (
                                 <div
-                                    onClick={() => toggleAnswer(item.key)}
-                                    className="faqpage-content-question"
+                                    key={item.key}
+                                    className="faqpage-content"
                                 >
-                                    <h3>{item.question}</h3>
-                                    <span>{isOpen ? '-' : '+'}</span>
+                                    <div
+                                        onClick={() => toggleAnswer(item.key)}
+                                        className="faqpage-content-question"
+                                    >
+                                        <h3>{item.question}</h3>
+                                        <span>{isOpen ? '-' : '+'}</span>
+                                    </div>
+                                    <div className={isOpen ? 'faqpage-content-answer-show' : 'faqpage-content-answer'}>
+                                        <p>{item.answer}</p>
+                                    </div>
                                 </div>
-                                <div className={isOpen ? 'faqpage-content-answer-show' : 'faqpage-content-answer'}>
-                                    <p>{item.answer}</p>
+                            )
+                        })
+                    }
+                </div>
+                <div>
+                    {
+                        faqItems2.map((item) => {
+                            const isOpen = item.key === openIndexes;
+                            return (
+                                <div
+                                    key={item.key}
+                                    className="faqpage-content"
+                                >
+                                    <div
+                                        onClick={() => toggleAnswer(item.key)}
+                                        className="faqpage-content-question"
+                                    >
+                                        <h3>{item.question}</h3>
+                                        <span>{isOpen ? '-' : '+'}</span>
+                                    </div>
+                                    <div className={isOpen ? 'faqpage-content-answer-show' : 'faqpage-content-answer'}>
+                                        <p>{item.answer}</p>
+                                    </div>
                                 </div>
-                            </div>
-                        )
-                    })
-                }
-
+                            )
+                        })
+                    }
+                </div>
             </div>
-            <ServicesContent />
+            <div className='faqpage-services-content'>
+                <ServicesContent />
+            </div>
         </div>
     )
 }
